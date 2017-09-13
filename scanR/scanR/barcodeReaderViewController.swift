9f41d3ef9d2ea3f92a60fd0c7e040fe8303a7d1e
#MEMETEAM <swiftgroup1@imac-server.local>
2017-09-14 10:34:37 +1200

    add comments and some other junk

--- scanR/scanR/barcodeReaderViewController.swift
+++ scanR/scanR/barcodeReaderViewController.swift
@@ -19,9 +19,14 @@ class BarcodeReaderViewController: UIViewController, AVCaptureMetadataOutputObje
     override func viewDidLoad() {
         super.viewDidLoad()
     
+         // Create a session object.
         session = AVCaptureSession()
+        
+        // Set the captureDevice.
         let videoCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        // Create input object.
         let videoInput: AVCaptureDeviceInput?
+        // Create output object.
         let metadataOutput = AVCaptureMetadataOutput()
         
         do {
@@ -30,16 +35,20 @@ class BarcodeReaderViewController: UIViewController, AVCaptureMetadataOutputObje
             return
         }
         
+        // Add input to the session.
         if (session.canAddInput(videoInput)) {
             session.addInput(videoInput)
         } else {
             scanningNotPossible()
         }
         
+        // Add output to the session.
         if (session.canAddOutput(metadataOutput)) {
             session.addOutput(metadataOutput)
             
+            // Send captured data to the delegate object via a serial queue.
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            // Set barcode type for which to scan: Code 39.
             metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeCode39Code]
             
         } else {
@@ -61,5 +70,24 @@ class BarcodeReaderViewController: UIViewController, AVCaptureMetadataOutputObje
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
         session = nil
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        
+        // Get the first object from the metadataObjects array.
+        if let barcodeData = metadataObjects.first {
+            // Turn it into machine readable code
+            let barcodeReadable = barcodeData as? AVMetadataMachineReadableCodeObject;
+            if let readableCode = barcodeReadable {
+                // Send the barcode as a string to barcodeDetected()
+                barcodeDetected(readableCode.stringValue);
+            }
+            
+            // Vibrate the device to give the user some feedback.
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            
+            // Avoid a very buzzy device.
+            session.stopRunning()
+        }
     }
 }
