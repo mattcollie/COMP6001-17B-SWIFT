d00f89d57175bbecf0610b5b4aa37a4cb8b666ba
Matt Collecutt <mattcollie@live.com>
2017-09-20 09:51:05 +1200

    adding apiLogic class and methods

---
+++ scanR/scanR/ApiLogic.swift
@@ -0,0 +1,59 @@
+class ApiLogic {
+    
+    class var endpoint: String { return "" }
+
+    typealias GetMethodHandler = (_: String?, _: Any?, _: Any?) -> Void
+
+    private static func GetURL(type: String) -> Any {
+        //let url = URL(string: "http://localhost:2344/api/" + type)!
+        //return url
+        return ""
+    }
+
+    internal static func GetRequest(response: GetMethodHandler) {
+        //let url = GetURL(endpoint)
+        //URLSession.shared.dataTask(with: url, completionHandler:  response).resume()
+        response(endpoint, "", "")
+    }
+
+
+    public static func Get(response: GetMethodHandler) {
+        GetRequest(response: response)
+    }
+}
+
+class StudentApi : ApiLogic {
+    
+    override class var endpoint: String { return "student" }
+
+}
+
+class TimesheetApi : ApiLogic {
+    
+    override class var endpoint: String { return "timesheet" }
+
+}
+
+struct Timeslot {
+    var Id: Int64
+    var Day: Int
+    var Hour: Int
+    var DurationMinutes: Int
+    var ClassName: String
+    var PaperName: String
+    var ClassType: String
+    var StudentId: Int64
+}
+
+struct Student {
+    var StudentId: Int64
+    var FirstName: String
+    var LastName: String
+    var BarcodeId: Int64?
+}
+
+TImesheetApi.Get(response:  { (data, response, error) in
+    if let data = data {
+        print(data)
+    }
+})
